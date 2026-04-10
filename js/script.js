4576f496817d702e2a966df3d114b68557bc5df2
TeresaH16 <heredia.t@icloud.com>
2026-04-10 14:50:28 -0400

    adding elements to js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,42 @@
+// Input and button elements - TH
+const searchBtn = document.getElementById("searchBtn");
+const errorMessage = document.getElementById("errorMessage");
+// Main sections that will be shown/hidden 
+const welcomeCard = document.getElementById("welcomeCard");
+const locationSection = document.getElementById("locationSection");
+const currentWeatherSection = document.getElementById("currentWeatherSection");
+const insightSection = document.getElementById("insightSection");
+const forecastSection = document.getElementById("forecastSection");
+// Elements for location and condition info
+const locationName = document.getElementById("locationName");
+const localTime = document.getElementById("localTime");
+const conditionIcon = document.getElementById("conditionIcon");
+const conditionText = document.getElementById("conditionText");
+// Elements for current weather details 
+const temperature = document.getElementById("temperature");
+const feelsLike = document.getElementById("feelsLike");
+const humidity = document.getElementById("humidity");
+const wind = document.getElementById("wind");
+const uv = document.getElementById("uv");
+const pressure = document.getElementById("pressure");
+// Elements for sun/daylight details 
+const sunrise = document.getElementById("sunrise");
+const sunset = document.getElementById("sunset");
+const chanceOfRain = document.getElementById("chanceOfRain");
+const maxMin = document.getElementById("maxMin");
+// Eleements for insight text, forecast cards and recent searches
+const weatherInsight = document.getElementById("weatherInsight");
+const weeklyForecast = document.getElementById("weeklyForecast");
+const recentSearches = document.getElementById("recentSearches");
+
+
+
+
 
 
 async function getWeather() {
     // Get the location from the input field -JM
-    const location = document.getElementById('locationInput').value;
+    const locationInput = document.getElementById('locationInput').value;
     //API Key for WeatherAPI -JM
     const apiKey = "d9f588f23e944e0bb6a182418260804";
 
@@ -26,7 +60,7 @@ async function getWeather() {
         } else {
             document.getElementById('locationName').innerHTML = 
             `<h2>${location}</h2>
-<p>${data.location.region}, ${data.location.country}</p>
+            <p>${data.location.region}, ${data.location.country}</p>
             <img src="${data.current.condition.icon}" alt="Weather Icon">`;
         }
         
@@ -41,7 +75,7 @@ async function getWeather() {
     <p>Condition: ${data.current.condition.text}</p>
     <p>Humidity: ${data.current.humidity}%</p>
     <p>Wind: ${data.current.wind_kph} kph</p>
-`;
+    `;
     // change color background according to weather condition
     if (condition.includes("sun")) {
         document.body.style.background = "linear-gradient(to right, #fceabb, #f8b500)";
